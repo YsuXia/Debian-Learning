e26d49abb67d15e550ee987cce8a5178f53d4a94
XiaYang <xiayang_yanshan@163.com>
2017-03-23 19:41:37 +0800

    change at company

---
+++ Linux-Kernel.mm
@@ -0,0 +1,20 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1490259697760" ID="ID_1034780742" MODIFIED="1490259794625">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <b><font size="4">Linux Kernel&#23398;&#20064;</font></b>
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1490259736385" ID="ID_1413497133" MODIFIED="1490259797536" POSITION="right" TEXT="Linux Kernel Development 3">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1490259752393" ID="ID_1153863074" MODIFIED="1490259779002" TEXT="&#x7b2c;&#x4e09;&#x7ae0;&#xff1a; &#x8fdb;&#x7a0b;&#x7ba1;&#x7406;"/>
+</node>
+</node>
+</map>
