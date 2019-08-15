27c9f162935866a9585d8f2e81619b379035d774
xiayang <xiayang_yanshan@163.com>
2019-08-15 22:17:57 +0800

    modify at 0815

---
+++ Android-App-0815.mm
@@ -0,0 +1,227 @@
+<map version="1.1.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1553787312228" ID="ID_1717231216" MODIFIED="1553787360865">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <b><font color="#1116f9">Android App&#x5f00;&#x53d1;&#x5b66;&#x4e60;</font></b>
+    </p>
+  </body>
+</html></richcontent>
+<node CREATED="1553787463112" ID="ID_832448031" MODIFIED="1553787511470" POSITION="right">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <b>&#x7b2c;&#x4e00;&#x90e8;&#x5206;&#xff1a; &#x4f7f;&#x7528;android studio&#x5f00;&#x53d1;app</b>
+    </p>
+  </body>
+</html></richcontent>
+<node CREATED="1553788932890" ID="ID_1566171347" MODIFIED="1553788987715">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      &#x8ba4;&#x8bc6;&#x548c;&#x5b66;&#x4e60;&#x7b2c;&#x4e00;&#x4e2a;app&#x7684;&#x5f00;&#x53d1;&#x8fc7;&#x7a0b;
+    </p>
+    <p>
+      https://developer.android.com/training/basics/firstapp
+    </p>
+  </body>
+</html></richcontent>
+<node CREATED="1553789477341" ID="ID_675768749" MODIFIED="1553789491680">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <img src="android-app/android&#x5de5;&#x7a0b;&#x754c;&#x9762;&#x5e03;&#x5c40;.png"/>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1553790071930" ID="ID_110813041" MODIFIED="1553790087922" TEXT="&#x5982;&#x4f55;&#x7f16;&#x8bd1;&#x5de5;&#x7a0b;&#x548c;&#x8fd0;&#x884c;&#x5de5;&#x7a0b;"/>
+<node CREATED="1553790094522" ID="ID_583912259" MODIFIED="1553790107452" TEXT="&#x6784;&#x5efa;&#x4e00;&#x4e2a;&#x7b80;&#x5355;&#x7684;&#x7528;&#x6237;&#x63a5;&#x53e3;">
+<node CREATED="1553790839589" ID="ID_1882560273" MODIFIED="1553790932079">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      android app&#x7684;&#x7528;&#x6237;&#x63a5;&#x53e3;&#x662f;&#x901a;&#x8fc7;layouts(viewgroup object)&#x548c;widgets&#xff08;view object&#xff09;
+    </p>
+    <p>
+      &#x6765;&#x5c42;&#x6b21;&#x5316;&#x6784;&#x5efa;&#x5b8c;&#x6210;&#x7684;
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1553788597399" ID="ID_1459334079" MODIFIED="1553788637999" POSITION="left">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <b>&#x7b2c;&#x4e8c;&#x90e8;&#x5206;&#xff1a; android &#x56db;&#x5927;&#x7ec4;&#x4ef6;&#x5b66;&#x4e60;</b>
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1562393881838" ID="ID_275492165" MODIFIED="1562393921544" POSITION="right">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <b>&#31532;&#19977;&#37096;&#20998;&#65306; java&#35821;&#35328;&#23398;&#20064;</b>
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1562468496897" ID="ID_1101698533" MODIFIED="1562468506573" TEXT="java&#x7684;&#x57fa;&#x672c;&#x7a0b;&#x5e8f;&#x8bbe;&#x8ba1;&#x7ed3;&#x6784;">
+<node CREATED="1562393929485" ID="ID_1904735963" MODIFIED="1562467985318">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      java&#30340;&#25968;&#25454;&#31867;&#22411;
+    </p>
+    <p>
+      8&#31181;&#22522;&#26412;&#31867;&#22411;&#65292;4&#20010;&#25972;&#22411;&#65292;2&#20010;&#28014;&#28857;&#31867;&#22411;&#65292;1&#20010;&#23383;&#31526;&#31867;&#22411;char(unicode&#32534;&#30721;)&#65292;&#19968;&#20010;&#24067;&#23572;&#22411;boolean
+    </p>
+    <p>
+      int 4&#23383;&#33410; short 2&#23383;&#33410; long 8&#23383;&#33410; byte 1&#23383;&#33410; java&#24403;&#20013;&#27809;&#26377;unsigned&#31867;&#22411;&#30340;&#25972;&#25968;
+    </p>
+    <p>
+      float 4&#23383;&#33410;&#160;&#160;double 8&#23383;&#33410;
+    </p>
+    <p>
+      char&#31867;&#22411;&#37319;&#29992;UTF-16&#32534;&#30721;&#26684;&#24335;
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1562468078211" ID="ID_965320358" MODIFIED="1562468124911">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      java&#35821;&#35328;&#20351;&#29992;final&#22768;&#26126;&#19968;&#20010;&#24120;&#37327;&#65292;&#24120;&#37327;&#19968;&#33324;&#20351;&#29992;&#22823;&#20889;&#30340;&#26041;&#24335;&#65292;
+    </p>
+    <p>
+      &#24182;&#19988;&#24120;&#37327;&#21482;&#33021;&#34987;&#36171;&#20540;&#19968;&#27425;
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1562468633336" ID="ID_1708502431" MODIFIED="1562468867051">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      &#20174;jdk5.0&#24320;&#22987;&#24341;&#29992;math&#31867;&#21487;&#20197;&#20570;&#22914;&#19979;&#22788;&#29702;&#65306;
+    </p>
+    <p>
+      import static java.lang.Math.*;
+    </p>
+    <p>
+      &#22914;&#26524;&#24471;&#21040;&#19968;&#20010;&#23436;&#20840;&#21487;&#39044;&#27979;&#30340;&#32467;&#26524;&#27604;&#36816;&#31639;&#36895;&#24230;&#26356;&#37325;&#35201;&#30340;&#35805;&#65292;&#21487;&#20197;
+    </p>
+    <p>
+      &#20351;&#29992;StrictMath&#31867;&#65292;&#23427;&#20351;&#29992;&#8220;&#33258;&#30001;&#21457;&#24067;&#30340;Math&#24211;(fdlibm)&quot;&#23454;&#29616;&#31639;&#27861;&#65292;
+    </p>
+    <p>
+      &#36825;&#26679;&#21487;&#20197;&#30830;&#20445;&#22312;&#25152;&#26377;&#24179;&#21488;&#19978;&#24471;&#21040;&#30456;&#21516;&#30340;&#32467;&#26524;&#12290;
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1562468950195" ID="ID_1154116875" MODIFIED="1562469143413">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      &#24378;&#21046;&#31867;&#22411;&#36716;&#25442;&#65306;
+    </p>
+    <p>
+      double x = 9.997;
+    </p>
+    <p>
+      int nx = (int)x;
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      &#22914;&#26524;&#24819;&#23545;&#28014;&#28857;&#25968;&#36827;&#34892;&#33293;&#20837;&#36816;&#31639;&#65292;&#24471;&#21040;&#26368;&#25509;&#36817;&#30340;&#25972;&#25968;&#65292;&#21487;&#20197;&#20351;&#29992;Math.round&#26041;&#27861;&#65292;
+    </p>
+    <p>
+      double x = 9.997;
+    </p>
+    <p>
+      int nx = (int)Math.round(x);
+    </p>
+    <p>
+      Math.round&#36820;&#22238;&#30340;&#26159;long&#31867;&#22411;&#30340;&#25972;&#25968;
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1562469215137" ID="ID_1694508928" MODIFIED="1562469305765">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      &#20174;jdk5.0 &#24320;&#22987;&#21487;&#20197;&#33258;&#23450;&#20041;&#26522;&#20030;&#31867;&#22411;&#65292;
+    </p>
+    <p>
+      enum Size(SMALL, MEDIUM, LARGE, EXTRA_LARGE);
+    </p>
+    <p>
+      Size s = SIze.MEDIUM;
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+</node>
+<node CREATED="1562489843445" ID="ID_572681833" MODIFIED="1562489880271" POSITION="left">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <b>&#29305;&#21035;&#31456;&#33410;: Camera App&#24320;&#21457;</b>
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+</map>
